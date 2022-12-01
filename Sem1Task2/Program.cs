7197a9e6b0d1cf586ea1bd0137cb43e5ba0bc366
Dmitriy <901170@mail.ru>
2022-12-01 22:03:11 +0500

    Comparing numbers

---
+++ Sem1Task2/Program.cs
@@ -0,0 +1,23 @@
+﻿// Считываем данные с консоли
+string? numLineA = Console.ReadLine();
+string? numLineB = Console.ReadLine();
+
+// Проверяем чтобы данные были не пустыми
+if (numLineA != null && numLineB != null)
+{
+    // Парсим введённое число
+int numA = int.Parse(numLineA);
+int numB = int.Parse(numLineB);
+// Сравниваем значения
+if (numA>numB)
+{
+    // Выводим данные в консоль
+ Console.WriteLine("Первое число больше второго");   
+}
+else
+{
+    Console.WriteLine("Первое число меньше второго"); 
+}
+
+
+}
