5ac4b6b27994379cbd188fad286324df94d4cfea
Dmitriy <901170@mail.ru>
2022-12-03 17:34:29 +0500

    Maximum number

---
+++ Sem1Task4/Program.cs
@@ -0,0 +1,45 @@
+﻿// Считываем данные с консоли
+string? numLineA = Console.ReadLine();
+string? numLineB = Console.ReadLine();
+string? numLineC = Console.ReadLine();
+
+// Проверяем чтобы данные были не пустыми
+if (numLineA != null && numLineB != null && numLineC != null)
+{
+
+    // Парсим введённое число
+int num1 = int.Parse(numLineA);
+int num2 = int.Parse(numLineB);
+int num3 = int.Parse(numLineC);
+
+// Сравниваем значения
+if (num1>num2)
+{
+   if (num1>num3) 
+   // Выводим данные в консоль
+    {
+     Console.WriteLine("max"+num1); 
+    }
+   
+    else 
+
+   // Выводим данные в консоль
+    {
+        Console.WriteLine("max"+num3); 
+    }
+}
+else
+{
+    if (num2>num3)
+   // Выводим данные в консоль
+    {
+        Console.WriteLine("max"+num2); 
+    }
+    else 
+
+  // Выводим данные в консоль
+    {
+        Console.WriteLine("max"+num3); 
+    }
+}
+}
