1eb059693c255499091e5a9750157f3f25c3f165
Dmitriy <901170@mail.ru>
2022-12-03 17:22:50 +0500

    Even numbers

---
+++ Sem1Task8/Program.cs
@@ -0,0 +1,32 @@
+﻿// Считываем данные с консоли
+string? numLineN  = Console.ReadLine();
+
+// Проверяем чтобы данные были не пустыми
+if (numLineN != null)
+{
+// Парсим введённое число
+int numN = int.Parse(numLineN);
+// Вводим переменные
+int firstNumber = 2;
+string results = string.Empty;
+// Запускаем цикл
+while (firstNumber<numN)
+{
+   results = results + firstNumber + "," ;
+   firstNumber = firstNumber + 2 ;
+}
+// Проверяем на чётность число N
+if (numN%2==0)
+{
+    results=results+numN;
+
+    // Выводим данные в консоль
+    Console.WriteLine(results);   
+}
+else
+{
+
+    // Выводим данные в консоль
+    Console.WriteLine(results);   
+}
+}
