e8979cb07adb60a7a174a7617061bfcb3b4fbffb
Dmitriy <901170@mail.ru>
2022-12-03 15:48:59 +0500

    Even odd

---
+++ Sem1Task6/Program.cs
@@ -0,0 +1,23 @@
+﻿// Считываем данные с консоли
+string? numLineA = Console.ReadLine();
+
+// Проверяем чтобы данные были не пустыми
+if (numLineA != null)
+{
+    // Парсим введённое число
+int numA = int.Parse(numLineA);
+int numB=numA%2;
+
+// Сравниваем значения
+if (numB==0)
+{
+    // Выводим данные в консоль
+    Console.WriteLine("Чётное");   
+}
+else
+{
+    Console.WriteLine("нечётное"); 
+}
+
+
+}
