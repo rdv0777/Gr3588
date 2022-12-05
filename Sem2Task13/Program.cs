e9c5256321ccd4a9248f80e8dc0bbfcf0ee66e45
Dmitriy <901170@mail.ru>
2022-12-05 20:59:22 +0500

    Third number

---
+++ Sem2Task13/Program.cs
@@ -0,0 +1,17 @@
+﻿
+// Считываем данные с консоли. Проверяем чтобы данные были не пустыми
+int num =int.Parse(Console.ReadLine()??"0");
+
+// Проверяем, чтобы число было трехзначным
+if ( num>99)
+{
+    
+Char[] digits = num.ToString().ToCharArray();
+
+   // Выводим данные в консоль
+      Console.WriteLine(digits[2] );
+}    
+else
+{
+     Console.WriteLine("Данное число не является трехзначным" );   
+}
