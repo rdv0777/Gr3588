716d0d51eb70c31c6d732d967a6f331d41ddee48
Dmitriy <901170@mail.ru>
2022-12-08 21:14:24 +0500

    Palindrome

---
+++ Sem3Task19/Program.cs
@@ -0,0 +1,31 @@
+﻿
+int num = ReadData("Введите пятизначный полиндром: ");
+
+bool res = PalinTest(num);
+
+PrintData(res);
+
+// Метод читает данные от пользователя
+int ReadData(string msg)
+{
+    Console.WriteLine(msg);
+    return int.Parse(Console.ReadLine()??"0");
+}
+
+bool PalinTest(int num)
+{
+    bool res =false;
+    res = (num/10000==num%10)&&((num/100)%10==(num/10)%10);
+    return res;
+}
+
+
+// Метод вывода данных
+void PrintData( bool res)
+{
+    Console.WriteLine(res);
+}
+
+
+
+
