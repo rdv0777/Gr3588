3a6ce870970f9520513a39f073b4c91ad4e7cdac
Dmitriy <901170@mail.ru>
2022-12-08 21:29:39 +0500

    Distance between points

--- Sem3Task20/Program.cs
+++ Sem3Task20/Program.cs
@@ -2,8 +2,10 @@
 int X2 = ReadData("Введите координату X2");
 int Y1 = ReadData("Введите координату Y1");
 int Y2 = ReadData("Введите координату Y2");
+int Z1 = ReadData("Введите координату Z1");
+int Z2 = ReadData("Введите координату Z2");
 
-double result = DstanceBetweenPoints(X1,Y1,X2,Y2);
+double result = DstanceBetweenPoints(X1,Y1,X2,Y2,Z1,Z2);
 
 PrintData("Расстояние между точками", result);
 
@@ -15,9 +17,9 @@ int ReadData(string msg)
 }
 
 // Метод находит расстояние межу точками на плоскости
-double DstanceBetweenPoints(double X1,double Y1,double X2,double Y2)
+double DstanceBetweenPoints(double X1,double Y1,double Z1,double X2,double Y2,double Z2)
 {
-    double distance = Math.Sqrt((X1-X2)*(X1-X2)+(Y1-Y2)*(Y1-Y2));
+    double distance = Math.Sqrt((X1-X2)*(X1-X2)+(Y1-Y2)*(Y1-Y2)+(Z1-Z2)*(Z1));
     return distance;
 }
 
