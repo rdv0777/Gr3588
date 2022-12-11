2ad9ccdafbc94150761202f36aca3e7065744d7f
Dmitriy <901170@mail.ru>
2022-12-11 20:40:35 +0500

    Exponentiation

--- Sem4Task25/Program.cs
+++ Sem4Task25/Program.cs
@@ -1,16 +1,16 @@
-﻿
+﻿// Метод читает данные от пользователя
 int ReadData(string msg)
 {
     Console.WriteLine(msg);
     return int.Parse(Console.ReadLine()??"0");
 }
-
+// Метод вывода данных
 void PrintData(int res)
 {
     Console.WriteLine(res);
    
 }
-
+// Метод возведения в степень
 int Pow(int A, int B)
 {
    int res = 1;
