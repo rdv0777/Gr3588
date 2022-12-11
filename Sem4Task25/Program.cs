ddd67646d54fbe6c586bba49e9fe9308135c4101
Dmitriy <901170@mail.ru>
2022-12-11 19:47:36 +0500

    Exponentiation

---
+++ Sem4Task25/Program.cs
@@ -0,0 +1,31 @@
+﻿
+int ReadData(string msg)
+{
+    Console.WriteLine(msg);
+    return int.Parse(Console.ReadLine()??"0");
+}
+
+void PrintData(int res)
+{
+    Console.WriteLine(res);
+   
+}
+
+int Pow(int A, int B)
+{
+   int res = 1;
+   for(int i = 1; i <= B; i++)
+   {
+      res = A*res;
+   }
+   return res;
+}
+
+
+int numA = ReadData("Введите число A");
+
+int numB = ReadData("Введите число B");
+
+int res = Pow(numA, numB);
+
+PrintData(res);
