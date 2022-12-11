2a03497bc946f3edcb0a5976e141f55c39873278
Dmitriy <901170@mail.ru>
2022-12-11 20:42:18 +0500

    The sum of digits in a number

---
+++ Sem4Task27/Program.cs
@@ -0,0 +1,30 @@
+﻿// Метод читает данные от пользователя
+int ReadData(string msg)
+{
+    Console.WriteLine(msg);
+    return int.Parse(Console.ReadLine()??"0");
+}
+// Метод вывода данных
+void PrintData(int res)
+{
+    Console.WriteLine(res);
+   
+}
+// Метод суммирования чисел
+int SumDigit(int num)
+{
+   int res = 0;
+   while(num>0)
+   {
+      res += num%10;
+      num = num/10;
+   }
+   return res;
+}
+
+
+int num = ReadData("Введите число ");
+
+int res = SumDigit(num);
+
+PrintData(res);
