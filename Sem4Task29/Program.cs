86814f8de3a12e8e96cb866bd0655c186f3044cc
Dmitriy <901170@mail.ru>
2022-12-12 18:49:37 +0500

    Array

---
+++ Sem4Task29/Program.cs
@@ -0,0 +1,45 @@
+﻿int arrLen = ReadData("Введите длину массива");
+
+int num1 = ReadData("Введите наименьшее число массива");
+
+int num2 = ReadData("Введите наибольшее число массива");
+
+int[] arr = GenArr(arrLen);
+
+PrintData("Сгенерированный массив: ",arr);
+
+
+int ReadData(string msg)
+{
+    Console.WriteLine(msg);
+    return int.Parse(Console.ReadLine()??"0");
+}
+
+void PrintData(string res, int[] arr)
+{
+    Console.WriteLine(res);
+    PrintArr(arr);
+}
+
+int[] GenArr(int num)
+{
+    Random rnd =new Random();
+    int[] arr = new int[num];
+    for (int i = 0; i < arr.Length; i++)
+    {
+        arr[i] = rnd.Next(num1,num2);
+    }
+    return arr;
+}
+
+void PrintArr(int[] arr)
+{
+    Console.Write("[");
+    for(int i = 0; i < arr.Length-1; i++)
+    {
+        Console.Write(arr[i]+",");
+    }
+    Console.WriteLine(arr[arr.Length-1]+"]");
+}
+
+
