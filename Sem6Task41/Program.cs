b90e20c5cd5f747a60a41e6cd57fbab28b9d6b54
Dmitriy <901170@mail.ru>
2022-12-19 21:34:36 +0500

    The number of numbers greater than 0

---
+++ Sem6Task41/Program.cs
@@ -0,0 +1,26 @@
+﻿
+Console.Write("Введите элементы(через пробел):");
+int[] arr = Array.ConvertAll(Console.ReadLine().Split(), int.Parse);
+PrintData(CountPosNum(arr));
+
+
+//Метод выводящий на печать 
+void PrintData(int res)
+{
+    Console.WriteLine( "Колличество чисел больше 0: "+res);
+}
+
+//Метод нахождения положительных чисел
+int CountPosNum(int[] num)
+{
+    int count = 0;
+
+    for (int i = 0; i < arr.Length; i++)
+    {
+        if (arr[i] > 0)
+        {
+            count++;
+        }
+    }
+    return count;
+}
