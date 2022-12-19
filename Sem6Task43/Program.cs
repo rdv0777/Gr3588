a3e821bb3f5c292a9237e651c81667b945a79742
Dmitriy <901170@mail.ru>
2022-12-19 19:42:08 +0500

    Intersection point

--- Sem6Task43/Program.cs
+++ Sem6Task43/Program.cs
@@ -1,18 +1,19 @@
-﻿int k1=ReadData("Введите k1: ");
-int b1=ReadData("Введите b1: ");
-int k2=ReadData("Введите k2: ");
-int b2=ReadData("Введите b2: ");
-double x = (-b2 + b1)/(-k1 + k2);
-double y = k2 * x + b2;
+﻿double k1=ReadData("Введите k1: ");
+double b1=ReadData("Введите b1: ");
+double k2=ReadData("Введите k2: ");
+double b2=ReadData("Введите b2: ");
+double x =(b2-b1)/(k1-k2);
+double y = k1*(b2-b1)/(k1-k2)+b1;
 
 PrintData("Пересечение в точке: x "+x+ " y "+y);
 
-int ReadData(string msg)
+// Метод вывода данных
+double ReadData(string msg)
 {
     Console.Write(msg);
-    return int.Parse(Console.ReadLine()??"0");
+    return double.Parse(Console.ReadLine()??"0");
 }
-//Метод выводящий на печать данные пользователя
+//Метод выводящий на печать 
 void PrintData(string res)
 {
     Console.WriteLine(res);
