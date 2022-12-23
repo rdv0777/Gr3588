57ab0ac3b7ee6b9b74163079bbeb005e2bb78d5c
Dmitriy <901170@mail.ru>
2022-12-23 19:27:53 +0500

    Arithmetic mean

---
+++ Sem7Task52/Program.cs
@@ -0,0 +1,66 @@
+﻿
+// Метод вывода данных
+int ReadData(string line)
+{
+    Console.Write(line);
+    int number = int.Parse(Console.ReadLine() ?? "0");
+    return number;
+}
+//Универсальный метод генерации и заполнения двумерного массива
+int[,] Fill2DArray(int countRow, int countColumn, int topBorder, int downBorder)
+{
+    System.Random rand = new System.Random();
+    int[,] array2D = new int[countRow, countColumn];
+
+    for (int i = 0; i < countRow; i++)
+    {
+        for (int j = 0; j < countColumn; j++)
+        {
+            array2D[i, j] = rand.Next(topBorder, downBorder + 1);
+        }
+    }
+    return array2D;
+}
+//Универсальный метод печати двумерного массива
+void Print2DArray(int[,] matrix)
+{
+    for (int i = 0; i < matrix.GetLength(0); i++)
+    {
+        for (int j = 0; j < matrix.GetLength(1); j++)
+        {
+            Console.Write(matrix[i, j] + " ");
+        }
+        Console.WriteLine();
+    }
+}
+int[] AwgCol(int[,]arr)
+{
+    int[]awr = new int[arr.GetLength(1)];
+    for(int j=0;j<arr.GetLength(1);j++)
+    {
+        for(int i=0;i<arr.GetLength(0);i++)
+        {
+            awr[j]+= arr[i,j]; 
+        }   
+         awr[j]= awr[j]/arr.GetLength(0); 
+    }  
+    
+    return awr;
+}
+// выводим сгенерированный массив
+void Print1DArr (int[] arr)
+{
+    for (int i=0; i<arr.Length-1; i++)
+    {
+        Console.Write(arr[i] + "  "  );
+    }
+    Console.WriteLine(arr[arr.Length-1]);
+}
+
+
+int row = ReadData("Введите колличество строк ");
+int column = ReadData("Введите колличество столбцов ");
+int[,] arr2D = Fill2DArray(row, column, 10, 99);
+ Print2DArray(arr2D);
+int []res=AwgCol(arr2D);
+Print1DArr(res);
