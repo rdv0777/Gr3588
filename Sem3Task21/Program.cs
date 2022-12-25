993ff57da28c44de645386169ec7d4aa0c2856b5
Dmitriy <901170@mail.ru>
2022-12-25 18:55:19 +0500

    Points in 3D space

---
+++ Sem3Task21/Program.cs
@@ -0,0 +1,34 @@
+﻿int number = ReadData("Введите число: ");
+
+string outLine = LineBilder(number, 1);
+PrintData("",outLine);
+outLine = LineBilder(number, 3);
+PrintData("",outLine);
+
+// Метод вывода данных
+void PrintData(string msg, string res)
+{
+    Console.WriteLine(msg+res);
+}
+
+// Метод сборки строки
+string LineBilder(int n, int pow)
+{
+    string line = string.Empty;
+    for(int i=1; i<n; i++)
+    {
+        line = line + Math.Pow (i, pow)+" ";
+    }
+    line = line + Math.Pow(n,pow);
+    return line;
+}
+
+// Метод читает данные от пользователя
+int ReadData(string msg)
+{
+    Console.WriteLine(msg);
+    return int.Parse(Console.ReadLine()??"0");
+}
+
+
+
