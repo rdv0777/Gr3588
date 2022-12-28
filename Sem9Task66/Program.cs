64e692157d2560512f7ab975aa0e2fc9dad8c1ce
Dmitriy <901170@mail.ru>
2022-12-28 18:52:39 +0500

    Sum of numbers

---
+++ Sem9Task66/Program.cs
@@ -0,0 +1,26 @@
+﻿
+// Метод ввода данных
+int ReadData(string line)
+{
+    Console.Write(line);
+    int number = int.Parse(Console.ReadLine() ?? "0");
+    return number;
+}
+
+// Печать результата
+void PrintResult(int prefix)
+{
+    Console.Write(prefix);
+}
+
+//Рекурентный метод суммирования чисел
+int SumMNRec(int M, int N)
+{
+    if(M>=N)return N;
+    return M+SumMNRec(M+1,N);
+}
+
+int number1 = ReadData("Введите число M; ");
+int number2 = ReadData("Введите число N; ");
+int resultLine = SumMNRec(number1,number2);
+ PrintResult(resultLine);
